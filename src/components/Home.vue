bb0ba2779f7797846075402804372543583db28d
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-03-13 13:29:40 +0100

    First version of home component with picture in foreground

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container-fliud">
+        <img
+          alt="Vue logo"
+          class="img-fluid"
+          src="../../public/profile.jpg"
+        />
+        <h1>Software Developer</h1>
+        <p>This is a fun project I built while learning vue.js and bootstrap</p>
+        <button type="button" class="btn btn-primary btn-lg">
+          Contact
+        </button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.section {
+  padding-top: 10%;
+  padding-bottom: 10%;
+}
+
+.img-fluid{
+  max-width: 100%;
+  height: auto;
+}
+</style>
