5aafc9d622b6e1c2d0135f1b49ea2dbb99c1a497
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-03-14 12:23:32 +0100

    First version of Navigation arrows using svg / css animations

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <template>
-  <Navigation/>
-  <router-view/>
+  <Navigation />
+  <button class="arrow left position-absolute top-50 start-50">
+    <svg width="60px" height="80px" viewBox="0 0 50 80" xml:space="preserve">
+      <polyline
+        fill="none"
+        stroke="#FFFFFF"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        points="
+	45.63,75.8 0.375,38.087 45.63,0.375 "
+      />
+    </svg>
+  </button>
+  <button class="arrow right">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="60px"
+      height="80px"
+      viewBox="0 0 50 80"
+      xml:space="preserve"
+    >
+      <polyline
+        fill="none"
+        stroke="#FFFFFF"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        points="
+	0.375,0.375 45.63,38.087 0.375,75.8 "
+      />
+    </svg>
+  </button>
+  <router-view />
 </template>
 
 <script>
@@ -9,8 +42,8 @@ import Navigation from "./components/Navigation.vue";
 export default {
   name: "App",
   components: {
-    Navigation
-  }
+    Navigation,
+  },
 };
 </script>
 
@@ -28,5 +61,58 @@ body {
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
+}
+
+button {
+  -webkit-appearance: none;
+  background: transparent;
+  border: 0;
+  outline: 0;
+}
+
+svg {
+  padding: 5px;
+}
+
+.arrow {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  margin-top: -45px;
+  margin-left: -35px;
+  width: 70px;
+  height: 90px;
+}
+
+.left {
+  left: 10%;
+}
+
+.right {
+  left: 90%;
+}
+
+.left:hover polyline,
+.left:focus polyline {
+  stroke-width: 8;
+}
+
+.left:active polyline {
+  stroke-width: 6;
+  transition: all 100ms ease-in-out;
+}
+
+.right:hover polyline,
+.right:focus polyline {
+  stroke-width: 6;
+}
+
+.right:active polyline {
+  stroke-width: 8;
+  transition: all 100ms ease-in-out;
+}
+
+polyline {
+  transition: all 250ms ease-in-out;
 }
 </style>
