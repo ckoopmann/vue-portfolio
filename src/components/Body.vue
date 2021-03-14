32ee826681e995a372100b5ba5d4f525aac001fc
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-03-14 15:01:59 +0100

    Revolving change between views

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="row align-items-center ">
     <div class="col">
-      <div
-        id="carouselExampleControls"
-        class="carousel slide"
-        data-ride="carousel"
-      >
+      <div id="carouselExampleControls" class="carousel slide">
         <div class="carousel-inner">
           <router-view />
         </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="prev"
-        >
+        <router-link class="carousel-control-prev" :to="previousView">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="next"
-        >
+        </router-link>
+        <router-link class="carousel-control-next" :to="nextView">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
-        </a>
+        </router-link>
       </div>
     </div>
   </div>
@@ -40,3 +26,35 @@
   margin: 0px;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      viewList: ["/home", "/projects"],
+    };
+  },
+  computed: {
+    currentViewIndex() {
+      const currentPath = this.$route.path;
+      const index = this.viewList.findIndex((path) => path === currentPath);
+      console.log("Current Path: ", currentPath);
+      console.log("Current Index: ", index);
+      return index;
+    },
+    previousView() {
+      let previousIndex;
+      if (this.currentViewIndex === 0) {
+        previousIndex = this.viewList.length - 1;
+      } else {
+        previousIndex = this.currentViewIndex - 1;
+      }
+      return this.viewList[previousIndex];
+    },
+    nextView() {
+      const nextIndex = (this.currentViewIndex + 1) % this.viewList.length;
+      return this.viewList[nextIndex];
+    },
+  },
+};
+</script>
