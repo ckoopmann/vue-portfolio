b313eb86798c7bd6879801c3fe07343cdb90d08c
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-04-06 14:48:31 +0200

    Add profile picture, slogan and links to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="align-self-center">
     <div class="container-fluid">
-      <h1>Christian Koopmann</h1>
-      <p class="lead">This is my personal website.</p>
+      <img class="rounded-circle img-custome" src="assets/profile.jpg" />
+      <h1 class="display-3">Christian Koopmann</h1>
+      <p class="lead">
+        Data driven software engineer, eternal student and curious mind.
+      </p>
       <a type="button" class="btn btn-primary btn-lg" :href="`mailto:${email}`">
-        Contact
+        <i class="far fa-envelope fa-lg"></i>
+      </a>
+      <a type="button" class="btn btn-primary btn-lg ml-1" :href="linkedin">
+        <i class="fab fa-linkedin fa-lg"></i>
+      </a>
+      <a type="button" class="btn btn-primary btn-lg ml-1" :href="github">
+        <i class="fab fa-github fa-lg"></i>
       </a>
     </div>
   </div>
@@ -15,13 +24,31 @@ export default {
   data() {
     return {
       email: "c.k.e.koopmann@gmail.com",
+      linkedin: "https://www.linkedin.com/in/christian-koopmann-a97ab55b/",
+      github: "https://github.com/ckoopmann",
     };
   },
 };
 </script>
 
 <style scoped>
-h1, p {
+h1,
+p {
   color: white;
+}
+
+p {
+  font-size: 2rem;
+}
+
+#logo {
+  background-color: white;
+}
+
+.img-custome {
+  width: 200px;
+  height: 200px;
+  margin-top: 15px;
+  margin-bottom: 15px;
 }
 </style>
